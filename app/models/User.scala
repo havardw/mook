8a141aad85c5837575139ab1f19a6cd96091192a
Håvard Wigtil <havardw@users.noreply.github.com>
2014-01-04 22:38:36 +0000

    Fixed a bug with quoting for SQL strings.

--- app/models/User.scala
+++ app/models/User.scala
@@ -9,7 +9,7 @@ import anorm.SqlParser._
 object User {
 
   def getAuthenticatedUser(email: String, password: String) : Option[String]  = DB.withConnection { implicit c =>
-    SQL("select name from user where email = '{email}' and hash = SHA2('{password}', 512);").on(
+    SQL("select name from user where email = {email} and hash = SHA2({password}, 512);").on(
       'email -> email,
       'password -> password
     ).as(scalar[String].singleOpt)
