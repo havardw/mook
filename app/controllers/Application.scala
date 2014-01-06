5af8b9d848ab9b4a949148d0ecd4f76e2c5e1cd5
Håvard Wigtil <havardw@users.noreply.github.com>
2014-01-06 18:23:42 +0000

    Login form

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -1,6 +1,9 @@
 package controllers
 
 import play.api.mvc._
+import play.api.data._
+import play.api.data.Forms._
+import models.User
 
 object Application extends Controller {
 
@@ -8,4 +11,24 @@ object Application extends Controller {
     Redirect(routes.Entries.entries)
   }
 
+  val loginForm = Form(
+    tuple(
+      "email" -> email,
+      "password" -> text
+    ) verifying ("Invalid email or password", result => result match {
+      case (email, password) => User.getAuthenticatedUser(email, password).isDefined
+    })
+  )
+
+  def login = Action {
+    Ok(views.html.login(loginForm))
+  }
+
+  def handleLogin = Action { implicit request =>
+    val u = User.getAuthenticatedUser("havardw@wigtil.net", "test")
+    loginForm.bindFromRequest.fold(
+      formWithErrors => BadRequest(views.html.login(formWithErrors)),
+      user => Redirect(routes.Entries.entries).withSession("username" -> user._1) /* TODO Get use name instead of e-mail. */
+    )
+  }
 }
