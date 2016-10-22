bbf8d08f5377abab86983b676711428cd045230f
Håvard Wigtil <havardw@users.noreply.github.com>
2016-10-22 09:56:50 +0000

    Sanitized syntax so that this file can be used by HSQLDB.

--- mook.sql
+++ mook.sql
@@ -1,15 +1,21 @@
-CREATE TABLE `entry` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `entryDate` date NOT NULL,
-  `entryText` text NOT NULL,
-  `userId` int NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=32 DEFAULT CHARSET=utf8;
+CREATE TABLE entry (
+  id int(11) NOT NULL AUTO_INCREMENT,
+  entryDate date NOT NULL,
+  entryText text NOT NULL,
+  userId int NOT NULL,
+  PRIMARY KEY (id)
+);
 
-CREATE TABLE `user` (
-  `id` int NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  `name` varchar(40) NOT NULL,
-  `email` varchar(255) NOT NULL,
-  `hash` varchar(255) NOT NULL,
-  UNIQUE KEY `email` (`email`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+CREATE TABLE user (
+  id int NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  name varchar(40) NOT NULL,
+  email varchar(255) NOT NULL,
+  hash varchar(255) NOT NULL,
+  UNIQUE KEY (email)
+);
+
+CREATE TABLE image (
+  id int NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  userId int NOT NULL,
+  mimeType varchar(40) NOT NULL
+)
