b085f23a2087ffd53c01e662e70f15de0abef2a3
Håvard Wigtil <havardw@users.noreply.github.com>
2016-11-26 01:12:27 +0100

    Use system properties instead of JNDI to set environment, new app server setup doesn't support JNDI.

--- src/main/java/mook/BeanProducer.java
+++ src/main/java/mook/BeanProducer.java
@@ -1,24 +1,33 @@
 package mook;
 
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
 import lombok.extern.slf4j.Slf4j;
 
 import javax.enterprise.inject.Produces;
 import javax.inject.Singleton;
-import javax.naming.InitialContext;
-import javax.naming.NamingException;
 import javax.sql.DataSource;
 
 @Slf4j
 public class BeanProducer {
-    
+
     @Produces
     @Singleton
     DataSource sql() {
-        try {
-            InitialContext ic = new InitialContext();
-            return (DataSource)ic.lookup("java:comp/env/jdbc/MookDS");
-        } catch (NamingException e) {
-            throw new RuntimeException("DataSource lookup failed");
+        HikariConfig config = new HikariConfig();
+        config.setJdbcUrl(getRequiredProperty("mook.db.url"));
+        config.setUsername(getRequiredProperty("mook.db.username"));
+        config.setPassword(getRequiredProperty("mook.db.password"));
+
+        return new HikariDataSource(config);
+    }
+
+    private static String getRequiredProperty(String key) {
+        String value = System.getProperty(key);
+        if (value == null || value.trim().isEmpty()) {
+            throw new IllegalStateException(String.format("Required property %1$s not set, use -D%1$s=... on command line", key));
         }
+
+        return value;
     }
 }
