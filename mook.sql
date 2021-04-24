a68b0d0e5d891fb4f0641fb656f12ca5a2d131be
Håvard Wigtil <haavard.wigtil@kantega.no>
2021-04-24 08:33:18 +0200

    Added session cache introduced in 0.3.3.

--- mook.sql
+++ mook.sql
@@ -22,3 +22,9 @@ CREATE TABLE image (
   mimeType varchar(40) NOT NULL,
   caption text
 );
+
+CREATE TABLE userSession (
+  uuid varchar(40) NOT NULL PRIMARY KEY,
+  userId int NOT NULL,
+  expires timestamp NOT NULL
+);
